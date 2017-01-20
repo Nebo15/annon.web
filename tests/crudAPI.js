42c5f4f5db6cfa98c1f5c3830501c6038633893c
napster <evgeniy.furmann@gmail.com>
2017-01-20 16:17:38 +0200

    create/edit/delete API test

--- tests/crudAPI.js
+++ tests/crudAPI.js
@@ -2,9 +2,11 @@
 import faker from 'faker';
 
 module.exports = {
-  'create/delete API test': (client) => {
+  'create/edit/delete API test': (client) => {
     const apiName = faker.lorem.words(1);
     const hostName = faker.lorem.words(1);
+    const editApiName = faker.lorem.words(1);
+    const editHostName = faker.lorem.words(1);
 
     client.page.createPage().navigate().createApis({
       api: apiName,
@@ -13,6 +15,14 @@ module.exports = {
       path: 'test/test',
     });
     client.page.apisPage().apisList().assertNewApi(apiName);
+    client.page.apisPage().editApi();
+    client.page.editApiPage().assertEditPage().editAPI({
+      api: editApiName,
+      host: editHostName,
+      port: '8086',
+      path: 'edit/eddd',
+    });
+    client.page.apisPage().apisList().assertNewApi(editApiName);
     client.page.apisPage().editApi();
     client.page.editApiPage().assertEditPage().deleteApis();
     client.page.apisPage().apisList().assertEmptyList(apiName);
--- tests/pages/editApiPage.js
+++ tests/pages/editApiPage.js
@@ -9,6 +9,33 @@ module.exports = {
     confirmDeleteButton: {
       selector: '#api-edit-page button:nth-child(2)',
     },
+    apiNameInput: {
+      selector: 'input[name="name"]',
+    },
+    apiMethodPut: {
+      selector: 'label:nth-child(1) > span:nth-child(2) > i:nth-child(1)',
+    },
+    apiMethodPost: {
+      selector: 'label:nth-child(2) > span:nth-child(2) > i:nth-child(1)',
+    },
+    apiMethodGet: {
+      selector: 'label:nth-child(3) > span:nth-child(2) > i:nth-child(1)',
+    },
+    apiMethodDelete: {
+      selector: 'label:nth-child(4) > span:nth-child(2) > i:nth-child(1)',
+    },
+    hostInput: {
+      selector: 'input[name="request.host"]',
+    },
+    portInput: {
+      selector: 'input[name="request.port"]',
+    },
+    pathInput: {
+      selector: 'input[name="request.path"]',
+    },
+    saveForm: {
+      selector: 'button[type="submit"]:nth-child(8)',
+    },
   },
   commands: [{
     assertEditPage() {
@@ -23,6 +50,19 @@ module.exports = {
         .waitForElementPresent('@confirmDeleteButton')
         .click('@confirmDeleteButton')
         .click('@confirmDeleteButton');
+    },
+    editAPI({ api, host, port, path }) {
+      return this
+        .waitForElementPresent('@apiNameInput')
+        .clearValue('@apiNameInput')
+        .setValue('@apiNameInput', api)
+        .waitForElementPresent('@apiMethodPut')
+        .click('@apiMethodPut')
+        .click('@apiMethodDelete')
+        .setValue('@hostInput', host)
+        .setValue('@portInput', port)
+        .setValue('@pathInput', path)
+        .click('@saveForm');
     },
   }],
 };
