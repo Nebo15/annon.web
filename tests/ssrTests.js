100c2297ada1bb35ac60463e61d66683dadfb844
napster <evgeniy.furmann@gmail.com>
2017-01-20 15:13:20 +0200

    update

--- tests/ssrTests.js
+++ tests/ssrTests.js
@@ -1,15 +1,12 @@
 module.exports = {
   'apis ssr test': (client) => {
-    client.page.apisPage().navigate();
-    client.page.apisPage().apisList();
+    client.page.apisPage().navigate().apisList();
   },
   'create ssr test': (client) => {
-    client.page.createPage().navigate();
-    client.page.createPage().createApiSSR();
+    client.page.createPage().navigate().createApiSSR();
   },
   'requests ssr test': (client) => {
-    client.page.requestsPage().navigate();
-    client.page.requestsPage().requestsListSSR();
+    client.page.requestsPage().navigate().requestsListSSR();
   },
   after: (client) => {
     client.end();
