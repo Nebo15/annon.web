37c753bdb3c7fde59ca2968624f92d94bc341343
napster <evgeniy.furmann@gmail.com>
2017-01-20 15:13:20 +0200

    update

--- tests/crudAPI.js
+++ tests/crudAPI.js
@@ -6,20 +6,16 @@ module.exports = {
     const apiName = faker.lorem.words(1);
     const hostName = faker.lorem.words(1);
 
-    client.page.createPage().navigate();
-    client.page.createPage().createApis({
+    client.page.createPage().navigate().createApis({
       api: apiName,
       host: hostName,
       port: '9091',
       path: 'test/test',
     });
-    client.page.apisPage().apisList();
-    client.page.apisPage().assertNewApi(apiName);
+    client.page.apisPage().apisList().assertNewApi(apiName);
     client.page.apisPage().editApi();
-    client.page.editApiPage().assertEditPage();
-    client.page.editApiPage().deleteApis();
-    client.page.apisPage().apisList();
-    client.page.apisPage().assertEmptyList(apiName);
+    client.page.editApiPage().assertEditPage().deleteApis();
+    client.page.apisPage().apisList().assertEmptyList(apiName);
   },
   after: (client) => {
     client.end();
