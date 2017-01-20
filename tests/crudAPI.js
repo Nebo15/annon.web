52969ac7baaeedd0a8ca534f3737a804e1e7f2a7
napster <evgeniy.furmann@gmail.com>
2017-01-20 15:06:46 +0200

    revert test

--- tests/crudAPI.js
+++ tests/crudAPI.js
@@ -1,26 +1,26 @@
 
-// import faker from 'faker';
+import faker from 'faker';
 
 module.exports = {
-  // 'create/delete API test': (client) => {
-  //   const apiName = faker.lorem.words(1);
-  //   const hostName = faker.lorem.words(1);
-  //
-  //   client.page.createPage().navigate();
-  //   client.page.createPage().createApis({
-  //     api: apiName,
-  //     host: hostName,
-  //     port: '9091',
-  //     path: 'test/test',
-  //   });
-  //   client.page.apisPage().apisList();
-  //   client.page.apisPage().assertNewApi(apiName);
-  //   client.page.apisPage().editApi();
-  //   client.page.editApiPage().assertEditPage();
-  //   client.page.editApiPage().deleteApis();
-  //   client.page.apisPage().apisList();
-  //   client.page.apisPage().assertEmptyList(apiName);
-  // },
+  'create/delete API test': (client) => {
+    const apiName = faker.lorem.words(1);
+    const hostName = faker.lorem.words(1);
+
+    client.page.createPage().navigate();
+    client.page.createPage().createApis({
+      api: apiName,
+      host: hostName,
+      port: '9091',
+      path: 'test/test',
+    });
+    client.page.apisPage().apisList();
+    client.page.apisPage().assertNewApi(apiName);
+    client.page.apisPage().editApi();
+    client.page.editApiPage().assertEditPage();
+    client.page.editApiPage().deleteApis();
+    client.page.apisPage().apisList();
+    client.page.apisPage().assertEmptyList(apiName);
+  },
   after: (client) => {
     client.end();
   },
