98d5f945e26a0d52774dce65717ea80d96374023
napster <evgeniy.furmann@gmail.com>
2017-01-20 16:17:38 +0200

    create/edit/delete API test

--- tests/ssrTests.js
+++ tests/ssrTests.js
@@ -1,14 +1,14 @@
-module.exports = {
-  'apis ssr test': (client) => {
-    client.page.apisPage().navigate().apisList();
-  },
-  'create ssr test': (client) => {
-    client.page.createPage().navigate().createApiSSR();
-  },
-  'requests ssr test': (client) => {
-    client.page.requestsPage().navigate().requestsListSSR();
-  },
-  after: (client) => {
-    client.end();
-  },
-};
+// module.exports = {
+//   'apis ssr test': (client) => {
+//     client.page.apisPage().navigate().apisList();
+//   },
+//   'create ssr test': (client) => {
+//     client.page.createPage().navigate().createApiSSR();
+//   },
+//   'requests ssr test': (client) => {
+//     client.page.requestsPage().navigate().requestsListSSR();
+//   },
+//   after: (client) => {
+//     client.end();
+//   },
+// };
