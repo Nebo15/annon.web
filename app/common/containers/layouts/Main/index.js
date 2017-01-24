c454cfa42a2d3bde93e8320141483c98ca00ec5b
Satanpit <alex.hyrenko@gmail.com>
2017-01-24 15:15:59 +0200

    Add Nebo logo

--- app/common/containers/layouts/Main/index.js
+++ app/common/containers/layouts/Main/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import withStyles from 'nebo15-isomorphic-style-loader/lib/withStyles';
 
+import Icon from 'components/Icon';
 import Aside from 'containers/blocks/Aside';
 
 import styles from './styles.scss';
@@ -13,6 +14,9 @@ const App = ({ children }) => (
         { children }
       </div>
     </main>
+    <footer className={styles.footer}>
+      <Icon name="nebo15" />
+    </footer>
   </div>
 );
 export default withStyles(styles)(App);
